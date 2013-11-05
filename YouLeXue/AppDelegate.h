3e48b098aca7efd9874e523273a029594c694215
Carl <Carl@admins-MacBook-Pro.local>
2013-11-05 11:44:17 +0800

    init project

---
+++ YouLeXue/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YouLeXue
+//
+//  Created by Carl on 13-11-5.
+//  Copyright (c) 2013年 Carl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YouLeXue/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  YouLeXue
+//
+//  Created by Carl on 13-11-5.
+//  Copyright (c) 2013年 Carl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
