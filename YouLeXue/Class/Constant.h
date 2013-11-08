afd6dfd787777815dc3f73559af9f844edab8063
vedon <vedon.fu@gmail.com>
2013-11-08 23:53:37 +0800

    数据库

---
+++ YouLeXue/Class/Constant.h
@@ -0,0 +1,15 @@
+//
+//  Constant.h
+//  YouLeXue
+//
+//  Created by vedon on 8/11/13.
+//  Copyright (c) 2013 Carl. All rights reserved.
+//
+
+#ifndef YouLeXue_Constant_h
+#define YouLeXue_Constant_h
+
+#define Server_URL @"http://www.55280.com/jsonapi/"
+
+
+#endif
