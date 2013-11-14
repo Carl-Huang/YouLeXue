b3c5410f79da8169f75521ff06c1713fcdd82e02
vedon <vedon.fu@gmail.com>
2013-11-14 18:25:34 +0800

    view

---
+++ YouLeXue/View/QuestionView.h
@@ -0,0 +1,22 @@
+//
+//  QuestionView.h
+//  YouLeXue
+//
+//  Created by vedon on 13/11/13.
+//  Copyright (c) 2013 Carl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuestionView : UIView<UIWebViewDelegate>
+{
+    CGRect rect;
+}
+@property (strong ,nonatomic) UIWebView * quesTextView;
+@property (strong ,nonatomic) NSString * answerStr;
+
+
+@property (strong ,nonatomic) NSString * textViewText;
+
+-(void)initWebView:(NSString *)str;
+@end
